b9486d6545ea861faf4b8dff5244fd74da101b7e
prathyushayamala <prathyushayamala@missionrnd.com>
2016-03-15 19:27:08 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -22,5 +22,39 @@ struct node {
 };
 
 void sll_012_sort(struct node *head){
-	
+	struct node *temp;
+	int zero_count = 0, one_count = 0, two_count = 0;
+	temp = head;
+	while (temp != NULL){
+		if (temp->data == 0){
+			zero_count++;
+		}
+		else{
+			if (temp->data == 1){
+				one_count++;
+			}
+			else{
+				two_count++;
+			}
+		}
+		temp = temp->next;
+	}
+	temp = head;
+	while (temp != NULL){
+		while (zero_count != 0){
+			temp->data = 0;
+			temp = temp->next;
+			zero_count--;
+		}
+		while (one_count != 0){
+			temp->data = 1;
+			temp = temp->next;
+			one_count--;
+		}
+		while (two_count != 0){
+			temp->data = 2;
+			temp = temp->next;
+			two_count--;
+		}
+	}
 }
